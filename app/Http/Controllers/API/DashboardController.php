4b9e3171dc42efaf2d452cb3f024ff6b316e29de
Nimesh Katariya <nimesh.katariya23@gmail.com>
2025-09-30 17:37:39 +0530

    Add total admin amount

--- app/Http/Controllers/API/DashboardController.php
+++ app/Http/Controllers/API/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Ticket;
+use App\Models\Transaction;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Exception;
@@ -23,13 +24,33 @@ class DashboardController extends Controller
                 ->take(10)
                 ->get();
 
+            // Admin Calculate amounts
+            $adminPendingAmount = Transaction::whereIn('ticket_id', function ($query) {
+                    $query->select('id')->from('tickets');
+                })
+                ->where('status', 'pending')
+                ->sum('amount');
+
+            $adminCompletedAmount = Transaction::whereIn('ticket_id', function ($query) {
+                    $query->select('id')->from('tickets');
+                })
+                ->where('status', 'admin_credit')
+                ->sum('amount');
+
+            $totalAdminPendingAmount = $adminPendingAmount - $adminCompletedAmount;
+            if ($totalAdminPendingAmount < 0) {
+                $totalAdminPendingAmount = 0;
+            }
+
             return response()->json([
                 'total_tickets' => $totalTickets,
                 'pending_tickets' => $pendingTickets,
                 'completed_tickets' => $completedTickets,
                 'total_users' => $totalUsers,
                 'staff_members' => $staffMembers,
-                'latest_tickets' => $latestTickets
+                'latest_tickets' => $latestTickets,
+                'total_admin_pending_amount' => $totalAdminPendingAmount,
+
             ]);
         } catch (Exception $e) {
             return response()->json([
